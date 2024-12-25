913159ef0561e6c89dcfd4b7945754a0be58342d
m-watanabe-8 <m-watanabe@eighteen.co.jp>
2024-12-25 16:54:01 +0900

    ヘッダーメニューのアンカーの遷移設定を修正

--- frontend/src/components/Header/HeaderAccount.jsx
+++ frontend/src/components/Header/HeaderAccount.jsx
@@ -10,12 +10,7 @@ import { useCookies } from 'react-cookie';
 export function HeaderAccount() {
     const [cookies, setCookie, removeCookie] = useCookies();
     const token = cookies.accessToken
-    const [auth, setAuth] = useState(true);
-    const [anchorEl, setAnchorEl] = useState(null);
-
-    const handleChange = (event) => {
-        setAuth(event.target.checked);
-    };
+    const [anchorEl, setAnchorEl] = useState(null); // メニューの表示位置を決定する基準点を設定
 
     const handleMenu = (event) => {
         setAnchorEl(event.currentTarget);
@@ -26,67 +21,63 @@ export function HeaderAccount() {
     };
 
     return (
-        <>
-            {auth && (
-                <div>
-                    <IconButton
-                        size="large"
-                        aria-label="account of current user"
-                        aria-controls="menu-appbar"
-                        aria-haspopup="true"
-                        onClick={handleMenu}
-                        color="inherit"
-                    >
-                        <AccountCircleIcon />
-                    </IconButton>
-                    {token === void 0 ? (
-                        <Menu
-                        id="menu-appbar"
-                        sx={{ mt: '45px' }}
-                        anchorEl={anchorEl}
-                        anchorOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        open={Boolean(anchorEl)}
-                        onClose={handleClose}
-                        >
-                            <MenuItem onClick={handleClose}>
-                                <Button color="inherit" href="/login/">ログイン</Button>
-                            </MenuItem>
-                            <MenuItem onClick={handleClose}>
-                                <Button color="inherit" href="/signup/">新規登録</Button>
-                            </MenuItem>
-                        </Menu>
-                    ) : (
-                        <Menu
-                        id="menu-appbar"
-                        sx={{ mt: '45px' }}
-                        anchorEl={anchorEl}
-                        anchorOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        open={Boolean(anchorEl)}
-                        onClose={handleClose}
-                        >
-                            <MenuItem onClick={handleClose}>
-                                <Logout />
-                            </MenuItem>
-                        </Menu>
-                    )}
-                </div>
+        <div>
+            <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+            >
+                <AccountCircleIcon />
+            </IconButton>
+            {token === void 0 ? (
+                <Menu
+                id="menu-appbar"
+                sx={{ mt: '45px' }}
+                anchorEl={anchorEl}     // メニューの位置を設定
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}    // anchorElがnullでない場合にメニューが表示される
+                onClose={handleClose}
+                >
+                    <MenuItem onClick={handleClose}>
+                        <Button color="inherit" href="/login">ログイン</Button>
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                        <Button color="inherit" href="/signup">新規登録</Button>
+                    </MenuItem>
+                </Menu>
+            ) : (
+                <Menu
+                id="menu-appbar"
+                sx={{ mt: '45px' }}
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+                >
+                    <MenuItem onClick={handleClose}>
+                        <Logout />
+                    </MenuItem>
+                </Menu>
             )}
-        </>
+        </div>
     );
 }
